af75ea3f410da052555f994b7c85deee69736daf
surajkus <kushwahasuraj752@gmail.com>
2025-01-29 20:14:23 +0530

    fist programming

--- first.java
+++ first.java
@@ -1,7 +1,26 @@
-public class first {
+class first {
     public static void main(String[] arg)
     {
-        System.out.print("hello world");
+        char c ='A';
+        int num =c;
+        System.out.print(num);
     }
 
+}
+class hello{
+    public static void main(String args[])
+    {
+        byte  z = 123;
+        int num =z;
+        System.out.print(num);
+    }
+
+}
+class second{
+    public static void main (String args[])
+    {
+        int i  = 32;
+        float A = i;
+        System.out.println(A);
+    }
 }
