f28b5dd7a99b9427b6c4019ac19fb4af14664aa5
surajkus <kushwahasuraj752@gmail.com>
2025-02-02 11:48:09 +0530

    some changes

--- NumguessingG.java
+++ NumguessingG.java
@@ -1,3 +1,4 @@
+import java.net.SocketOption;
 import java.util.Random;
 import java.util.Scanner;
 // Number guessing game in java
@@ -10,19 +11,21 @@ public class NumguessingG {
         Scanner sc = new Scanner(System.in);
         int user_input,point= 100;
         boolean matched = false;
+        System.out.println("Welcome in number guessing Game  ");
+        System.out.println("Lets in game Think a number between 1 to 100");
         while (!matched) {
-            System.out.println("Enter a number");
+            System.out.println("Enter your guess");
             user_input = sc.nextInt();
             if (user_input == num) {
-                System.out.println("You win ! !");
+                System.out.println("Congratulations You win ! !");
                 System.out.println("Points : " + point);
                 matched = true;
             } else if (user_input > num) {
-                System.out.println("think a small number..");
+                System.out.println("oops! think a small number..");
                 point--;
             }
           else{
-              System.out.println("thing big");
+              System.out.println("oops! think big");
               point--;
             }
         }
