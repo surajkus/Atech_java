5e6a06bbfb5b52bebeeb5714bd5d0376dd9b5d0d
surajkus <kushwahasuraj752@gmail.com>
2025-02-02 11:29:18 +0530

    days 7

---
+++ NumguessingG.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumguessingG {
+
+    public static void main(String[] argus)
+    {
+        Random ram = new Random();
+        int num = ram.nextInt( 100);
+        Scanner sc = new Scanner(System.in);
+        int user_input,point= 100;
+        boolean matched = false;
+        while (!matched) {
+            System.out.println("Enter a number");
+            user_input = sc.nextInt();
+            if (user_input == num) {
+                System.out.println("You win ! !");
+                System.out.println("Points : " + point);
+                matched = true;
+            } else if (user_input > num) {
+                System.out.println("think a small number..");
+                point--;
+            }
+          else{
+              System.out.println("thing big");
+              point--;
+            }
+        }
+    }
+}
