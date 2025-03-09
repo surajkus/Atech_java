c260e5dedd9fc4364f2cb4b6aabaf9fe357797a3
surajkus <kushwahasuraj752@gmail.com>
2025-03-09 12:21:26 +0530

    day 19

--- array.java
+++ array.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class array {
-public static void main(String args[]){
+    public static void main(String args[]) {
 
 //    //declare the variables;
 //    int[] arr ={2,34,21,15,67,87,34,56,78,32};
 //    System.out.println(Arrays.toString(arr));
-    // (when you are direct write arr name inside the sout then print address of array not a arrays)
+        // (when you are direct write arr name inside the sout then print address of array not a arrays)
 //
 //
 //    // method of arrays in java;
@@ -36,33 +36,38 @@ public static void main(String args[]){
 //    int index= Arrays.binarySearch(array2,60);
 //    System.out.println(index);
 
-    // arraycopy (copy of array)-----
-    int[] arr3={1,2,3,4,5,6};
-    int[] arr4 = new int[5];
-    System.arraycopy(arr3,0,arr4,0,5);
-    System.out.println(Arrays.toString(arr4));
+        // arraycopy (copy of array)-----
+//        int[] arr3 = {1, 2, 3, 4, 5, 6};
+//        int[] arr4 = new int[5];
+//        System.arraycopy(arr3, 0, arr4, 0, 5);
+//        System.out.println(Arrays.toString(arr4));
 
-    //list arrays in java
+        //list arrays in java
 
-    String[] name={"suraj","kundan","harekrishna","kushal"};
-    System.out.println(Arrays.toString(name));
-    List<String> friend_name= Arrays.asList(name);
-    System.out.println(friend_name);
+//        String[] name = {"suraj", "kundan", "harekrishna", "kushal"};
+//        System.out.println(Arrays.toString(name));
+//        List<String> friend_name = Arrays.asList(name);
+//        System.out.println(friend_name);
 
 
-    // addition of arrays.
-   int[] arr5={1,2,3,4,1,8,9,10};
-   int   sum= 1;
-   for(int i=0 ; i<arr5.length;i++)
-   {
-       sum = sum + arr5[i];
-   }
-    System.out.println("sum of all numbers in arrays "+sum);
-for( int i: arr5)
-{
-    sum =sum * i;
-}
-    System.out.println("mutilple of all numbers in arrays " +sum);
+        // addition of arrays.
+        int[] arr5 = {1, 2, 3, 4, 1, 8, 9, 10};
+        int sum = 1;
+        for (int i = 0; i < arr5.length; i++) {
+            sum = sum * arr5[i];
+        }
+        System.out.println("multiply of all numbers in arrays " + sum);
+    }
 }
 
-}
+//  // multiply of arrays
+//     int[] arr6={1,2,3,4,1,8,9,10};
+//    int   sum2= 0;
+//
+//   for( int i: arr6)
+//   {
+//    sum =sum2 + i;
+//    }
+//    System.out.println("mutilple of all numbers in arrays " +sum);
+//    }
+
