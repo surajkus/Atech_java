a8ae4513653bfc73265036efe7b818bb90a4888b
surajkus <kushwahasuraj752@gmail.com>
2025-03-20 18:03:36 +0530

    day 22

---
+++ arraylist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("banana");
+        list.add("pine_apple");
+        list.add(1,"mango");//LSMPGOABP
+        list.add(1,"payaya");
+        list.add(0,"gavabha");
+        list.add(0,"orange");//OGSALPMBP
+        list.add(3,"lichi");
+        list.add(2,"shitafall");  //LSMAGOPBP
+
+        System.out.println(list);
+    }
+}
