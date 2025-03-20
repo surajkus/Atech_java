b911ed36d943968085a61acd0f1ece8634c0aba0
surajkus <kushwahasuraj752@gmail.com>
2025-03-20 18:06:12 +0530

    day 22

---
+++ Add_2dA.java
@@ -0,0 +1,20 @@
+//public class Add_2dA {
+//    public static void main(String[] args) {
+//        int[][] arr = {
+//                {2, 3, 4, 5, 8,1,99,233,790,849,890, 9},
+//                { 290, 39, 100, 439, 230},
+//                {18,20,40,53,33,199}
+//        };
+////        int[] min = arr[0];
+////        for(int[]  check:arr){
+////            for(int num:check){
+////
+////
+////                }
+////            }
+////            System.out.println("maximum number is = "+min);
+//
+//        }
+//    }
+//
+//}
