066b4425eb81067fc7bc9f5b2425cfa04e249a60
surajkus <kushwahasuraj752@gmail.com>
2025-03-20 18:32:55 +0530

    day 23

---
+++ getandset.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class getandset {
+    public static void main(String[] args){
+        // method setter and getter in java..
+        ArrayList<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("mongo");
+        System.out.println(list);
+        list.set(0,"banana");
+        list.set(1,"papaya");
+        list.add(0,"pineapple");
+        list.remove(2);
+        list.add(1,"gavabha");//pgb
+        System.out.println(list);
+    }                                   //pbagmp
+}
+
