2e519871d13bfaec48a31ded1fdcb5fdefc15e7d
surajkus <kushwahasuraj752@gmail.com>
2025-03-20 18:02:54 +0530

    day 22

---
+++ fill2D.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fill2D {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3,4,5,6,10},
+                {22,14,16,1,30},
+                {38,20,33,20,29,43}
+        };
+
+
+        for(int i=0;i<=arr.length;i++){
+
+
+
+
+        }
+
+    }
+}
