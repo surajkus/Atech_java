5f0aa08c17c5c822518658f45174359d4a4bc52a
surajkus <kushwahasuraj752@gmail.com>
2025-03-28 01:28:11 +0530

    day 27

---
+++ solat.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class solat {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter String");
+        String n= sc.nextLine();
+        String solat ="s";
+        String result = "";
+        for(int i=0;i<n.length();i++){
+            result += n.charAt(i) +solat;
+        }
+        System.out.println(result);
+    }
+}
